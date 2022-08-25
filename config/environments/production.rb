52c0bfbd3604bc87941fc05454e696e0e9404ac4
Utsubo256 <greeeentea256@gmail.com>
2022-08-25 18:11:13 +0900

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = ENV['APP_DOMAIN']
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => ENV['APP_DOMAIN'],
+    :domain         => host,
     :authentication => :plain,
   }
 
